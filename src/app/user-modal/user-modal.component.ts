e6f8f6e81c4d2befaf21edf3b3ba79f43b7fe7b7
dkanskcj <dkanskcj@likelion.org>
2022-08-05 18:07:05 +0900

    why error?

--- src/app/user-modal/user-modal.component.ts
+++ src/app/user-modal/user-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonModal, ModalController } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-user-modal',
@@ -8,17 +9,39 @@ import { ModalController } from '@ionic/angular';
 })
 export class UserModalComponent implements OnInit {
 
-  constructor(
-    private modalContorller: ModalController
-  ) { }
+    @ViewChild(IonModal) modal: IonModal;
+
+    message = '';
+    name:string = 'string';
+
+    cancel(){
+      this.modal.dismiss(null, 'cancel');
+    }
+    confirm(){
+      this.modal.dismiss(this.name, 'confirm');
+    }
+    onWillDismiss(event: Event){
+      const ev = event as CustomEvent<OverlayEventDetail<string>>;
+      if(ev.detail.role === 'confirm'){
+        this.message = 'Hello, ${ev.detail.data}!';
+      }
+    }
+  
+
+
+  constructor() {}
+
+  // constructor(
+  //   private modalContorller: ModalController
+  // ) { }
 
   ngOnInit(): void {
   }
-  
-  async createModal() {
-    const modal = await this.modalContorller.create({
-      component: UserModalComponent,
-    })
-    modal.present();
-  }
+
+  // async createModal() {
+  //   const modal = await this.modalContorller.create({
+  //     component: UserModalComponent,
+  //   })
+  //   modal.present();
+  // }
 }
