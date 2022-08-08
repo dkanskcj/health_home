db8b514a70bd19a4994c314fb54954ec65c7d5f4
dkanskcj <dkanskcj@likelion.org>
2022-08-08 16:04:59 +0900

    re

--- src/app/user-target/user-target.component.ts
+++ src/app/user-target/user-target.component.ts
@@ -132,6 +132,26 @@ export class UserTargetComponent implements OnInit {
 
 }
 
+
+// export class LineChartComponent {
+//   lineChartData: ChartDataSets[] = [
+//     { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
+//   ];
+//   lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+//   lineChartOptions = {
+//     responsive: true,
+//   };
+//   lineChartColors: Color[] = [
+//     {
+//       borderColor: 'black',
+//       backgroundColor: 'rgba(255,255,0,0.28)',
+//     },
+//   ];
+//   lineChartLegend = true;
+//   lineChartPlugins = [];
+//   lineChartType = 'line';
+  
+// }
 // export class LineChartComponent implements AfterViewInit {
 //   @ViewChild('lineCanvas') lineCanvas: ElementRef;
 //   lineChart: any;
